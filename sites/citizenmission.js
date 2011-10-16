509d2bc8e158729efc49a82c62008d9b76f19fb7
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-10-15 17:27:01 -0700

    hup

--- sites/citizenmission.js
+++ sites/citizenmission.js
@@ -6,15 +6,15 @@ var server = connect();
 	subDomani.forEach(function (domain) {
 		server.use(connect.vhost(domain+'.citizenmission.com', 
 			connect().use(function (req,res){
-				res.writeHead('200')
-				res.end('<h2>Yodal!</h2>')
+				res.writeHead('200');
+				res.end('<h2>Yodal!</h2>');
+						console.log(req.subdomains)
 			})
 		))
 	});
 	server.use(function (req,res){
 		res.writeHead('200', {'Content-Type': 'text/html'});
 		res.end('<h2>Boo!</h2>'+req.session.cookie.maxAge);
-		console.log(req.subdomains)
 	});
 
 	
