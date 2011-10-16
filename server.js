c38348b41c297eb13bf3eb9d76dd00dcc4d2e21f
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-10-15 20:10:08 -0700

    hup

--- server.js
+++ server.js
@@ -6,18 +6,18 @@ var domani = ['citizenmission.com'];
 var subs = ['rhetoric-report'];
 
 var server = connect();
-	server.use(connect.logger());
-	server.use(connect.favicon());
-	server.use(connect.cookieParser());
-	server.use(connect.session({secret: 'giddy gato' }));
-	domani.forEach(function (domain) {
-		server.use(vhost(domain, subs, _,
-			require('./sites/citizenmission')(connect)
-		))
-	});
-	server.use(function (req,res){
-		res.writeHead('200', {'Content-Type': 'text/html'});
-		res.end('<h2>Howdy!</h2>'+req.session.cookie.maxAge);
-	});
-	server.listen(process.env.NODE_ENV === 'production' ? 80 : 8000);
-	console.log('Listening on ' + server.address().port);
+		server.use(connect.logger());
+		server.use(connect.favicon());
+		server.use(connect.cookieParser());
+		server.use(connect.session({secret: 'giddy gato' }));
+		domani.forEach(function (domain) {
+			server.use(vhost(domain, subs, _,
+				require('./sites/citizenmission')(connect)
+			))
+		});
+		server.use(function (req,res){
+			res.writeHead('200', {'Content-Type': 'text/html'});
+			res.end('<h2>Howdy!</h2>'+req.session.cookie.maxAge);
+		});
+		server.listen(process.env.NODE_ENV === 'production' ? 80 : 8000);
+		console.log('Listening on ' + server.address().port);
