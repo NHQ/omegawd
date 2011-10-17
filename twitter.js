4b4b1c5e5e8f1679b8f750cb9f6dbb7777fb8d62
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-10-17 13:54:38 -0700

    trerror

--- twitter.js
+++ twitter.js
@@ -1,20 +1,16 @@
-var request = require('request')
-var uid = new Buffer('astromanies:michaeljackson2009', encoding='utf8').toString('base64');
-var sys = require('sys'),
-    twitter = require('twitter');
+var twitter = require('twitter');
+		var twit = new twitter({
+		    consumer_key: 'mw6Dw4adevPW0l67wHk3hw',
+		    consumer_secret: 'iW0KQprGmvzpTvY0KZuLONdHrdYi9FBErBRIZGH0CM',
+		    access_token_key: '46961216-ns6DWzNaAUDnKkVUZQTkqQgXfa0Z4SGss1ElQfafA',
+		    access_token_secret: 'MSY57uqMaIMOtsRSWLvNDfL9DxaZXbrXGFak679tA78'
+		});
 
-console.log(uid);
-
-var twit = new twitter({
-    consumer_key: 'mw6Dw4adevPW0l67wHk3hw',
-    consumer_secret: 'iW0KQprGmvzpTvY0KZuLONdHrdYi9FBErBRIZGH0CM',
-    access_token_key: '46961216-ns6DWzNaAUDnKkVUZQTkqQgXfa0Z4SGss1ElQfafA',
-    access_token_secret: 'MSY57uqMaIMOtsRSWLvNDfL9DxaZXbrXGFak679tA78'
-});
-
-twit.stream('statuses/filter', 'track=nodejs', function(stream) {
-    stream.on('data', function (data) {
-				console.log(data)
-//        sys.puts(sys.inspect(data));
-    });
-});
+		twit.stream('statuses/filter', {track: 'nodejs'}, function(stream) {
+		    stream.on('data', function (data) {
+						console.log(data)
+		    });
+				stream.on('error', function(err){
+					console.log(err)
+				})
+		});
