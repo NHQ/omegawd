94ffbf7c73ababea8686b0312551a386c185410d
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-10-17 15:44:23 -0700

    test

--- twitter.js
+++ twitter.js
@@ -1,14 +1,131 @@
-var twitter = require('twitter');
-		var twit = new twitter({
-		    consumer_key: 'mw6Dw4adevPW0l67wHk3hw',
-		    consumer_secret: 'iW0KQprGmvzpTvY0KZuLONdHrdYi9FBErBRIZGH0CM',
-		    access_token_key: '46961216-ns6DWzNaAUDnKkVUZQTkqQgXfa0Z4SGss1ElQfafA',
-		    access_token_secret: 'MSY57uqMaIMOtsRSWLvNDfL9DxaZXbrXGFak679tA78'
-		});
+var twitter = require('twitter'),
+		_ = require('underscore'),
+		client = require('redis');
 
-var track = ['occupyla', 'ows', 'occupydc', 'occupychicago', 'occupychi', 'occupysf', 'occupysd', 'occupyaustin', 'occupy'];
+var twit = new twitter({
+    consumer_key: 'mw6Dw4adevPW0l67wHk3hw',
+    consumer_secret: 'iW0KQprGmvzpTvY0KZuLONdHrdYi9FBErBRIZGH0CM',
+    access_token_key: '46961216-ns6DWzNaAUDnKkVUZQTkqQgXfa0Z4SGss1ElQfafA',
+    access_token_secret: 'MSY57uqMaIMOtsRSWLvNDfL9DxaZXbrXGFak679tA78'
+});
 
-		twit.stream('statuses/filter', {track: 'occupy'}, function(stream) {
+var track = {
+	'Wall Street': 'ows',
+	'New York City': 'nyc',
+	'Los Angeles': 'la',
+	Chicago: 'chi',
+	Houston: null,
+	Philadelphia: 'philly',
+	Pheonix: null,
+	'San Antonio': 'sananto',
+	'San Diego': 'sd',
+	Dallas: null,
+	'San Jose': 'sj',
+	Jacksonville: null,
+	Indianapolis: 'indy', 
+	'San Francisco': 'sf',
+	Austin: null,
+	Columbus: null,
+	'Fort Worth': null,
+	Charlotte: null,
+	Detroit: null,
+	'El Paso': 'ep',
+	Memphis: null,
+	Baltimore: null,
+	Boston: null,
+	Seattle: null,
+	DC: 'kstreet',
+	Nashville: null,
+	Denver:  null,
+	Louisville: null,
+	Milwaukee: null,
+	Portland: null,
+	'Las Vegas': 'lv',
+	'Oklahoma City': 'oklahoma',
+	Albuquerque: 'abq',
+	Tucson: null,
+	Fresno: null,
+	Sacramento: 'sac',
+	'Kansas City': 'kc',
+	Atlanta: null,
+	'Colorado Springs': 'cs',
+	Omaha: null,
+	Atlanta: 'atl',
+	Miami: null,
+	Cleveland: null,
+	Tulsa: null,
+	Oakland: null,
+	Minneapolis: null,
+	'New Orleans': 'no',
+	Pittsburgh:  null,
+	'St Louis': 'stl',
+	Cincinnati: null,
+	'Saint Paul': 'sp',
+	Lincoln: null,
+	Madions: null,
+	'Baton Rouge': null,
+	Spokane: null,
+	Cheyenne: null,
+	Rochester: null,
+	Birmingham: null,
+	'San Bernardino': null,
+	'Des Moines': null,
+	Boise: null,
+	Richmond: null,
+	Montgomery: null,
+	'Little Rock': null,
+	'Salt Lake City':'slc',
+	Tallahassee: null,
+	Providence: null,
+	Jackson: null,
+	Salem: null,
+	Topeka: null,
+	Springfield:  null,
+	Lansing: null,
+	Juneau: null,
+	Hartford: null,
+	Dover: null,
+	Hawaii: null,
+	Augusta: null,
+	Annapolis: null,
+	'Jefferson City': 'jc',
+	Montana: null,
+	'Carson City': 'cc',
+	Concord: null,
+	Trenton: null,
+	'Santa Fe': null,
+	Albany: null,
+	Bismarck: null,
+	Harrisburg: null,
+	Providence: null,
+	Columbia: null,
+	Pierre: null,
+	Montpelier: 'mtp',
+	Olympia: null,
+	Charleston: null
+	};
+	var tracklist = [];
+	var mapper = {}
+	
+	_.map(track, function(value, key, list){
+		mapper[key] = [];
+		var one = '#occupy'+key.replace(/\s/g,"");
+		tracklist.push(one);
+		mapper[key].push(one)
+		if (value){
+			two = '#occupy'+value;
+			tracklist.push(two);
+			mapper[key].push(two)
+		}
+	})
+	
+	console.log(tracklist.join(), mapper)
+
+	function switchBoard(data){
+		
+	}
+
+		twit.stream('statuses/filter', {track: '#occupy'}, function(stream) {
 		    stream.on('data', function (data) {
 						console.log(data)
 		    });
@@ -16,3 +133,4 @@ var track = ['occupyla', 'ows', 'occupydc', 'occupychicago', 'occupychi', 'occup
 					console.log(err)
 				})
 		});
+		
