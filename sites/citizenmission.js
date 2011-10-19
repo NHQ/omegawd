2feadabb0ea021e92099b34aa9041e9e59ad81b4
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-10-18 19:09:15 -0700

    evening

--- sites/citizenmission.js
+++ sites/citizenmission.js
@@ -13,10 +13,10 @@ module.exports = function(connect, _){
 	*/
 		server.use(function (req, res){
 			var ahem = occupy.ows.latest.length;
-			console.log(req.subdomains);
+			console.log(ahem);
 			var fn = jade.compile('h2 !{ahem}', {ahem : ahem});
 			res.writeHead('200', {'Content-Type': 'text/html'});
-			res.end(fn());
+			res.end(ahem);
 	});
 	return server
 }; 
