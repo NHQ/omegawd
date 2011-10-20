44a31e5f2253b2e7ea0842e47d03a8b47de636cc
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-10-19 20:00:32 -0700

    spfrd

--- sites/citizenmission.js
+++ sites/citizenmission.js
@@ -1,18 +1,34 @@
-var jade = require('jade'), redis = require('redis');
+var jade = require('jade'), redis = require('redis'), trackmap = require('../lib/trackmap.js'), _ = require('underscore');
 
-var client - redis.createCLient();
+var client = redis.createClient();
+var mapper = {}, html;
+
+_.each(trackmap, function(val,key){
+	mapper[key] = ['occupy'+key]
+	if(val)
+	mapper[key].push('occupy'+val)
+})
+
+_.each(trackmap, function(v,k){
+	html += '<a href="/'+k+'">'+k+'</a>'
+})
+console.log(html);
+
+_.map(trackmap, function(val, key){
+	
+})
 
 module.exports = function(connect, _){
 
 	var server = connect();
-	/*
+
 		server.use(connect.router(function(app){
 			app.get('/', function(req, res){
 				res.writeHead('200', {'Content-Type': 'text/html'});
-				res.end('<h2>Boo!</h2>'+req.session.cookie.maxAge);
+				res.end(html);
 			})
 		}));
-	*/
+
 		server.use(function (req, res){
 			var ahem = occupy.mapper.ows.latest[0];
 			console.log(ahem);
