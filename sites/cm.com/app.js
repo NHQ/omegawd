8e8c81715466f397c74d07a0437b294160767d05
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-10-23 15:37:40 -0700

    spfdr

--- sites/cm.com/app.js
+++ sites/cm.com/app.js
@@ -80,9 +80,9 @@ app.get('/occupy', vhost, function(req, res){
 		var tags = _.map(trackmap.mapTags(state), function(k){
 			return 'occupy'+k+':links';
 		});
-		if (state.toUpperCase() == 'NEW YORK'){tags.push('ows:links', 'occupywallstreet:links')}
+		if (state.toUpperCase().replace(/-/g, " ") == 'NEW YORK'){tags.push('ows:links', 'occupywallstreet:links')}
 		client.zunionstore(state+':links', tags.length, tags, function(e,r){
-			client.zrevrangebyscore(state+':links', '+inf', 3, function(e,r){
+			client.zrevrangebyscore(state+':links', '+inf', 1, function(e,r){
 				res.render('links', {
 			    title: 'Occupy Links:'+state,
 					locals: {links: _.map(r, function(links){return JSON.parse(links)})} // links going in as json, needs fix, use one link not array
