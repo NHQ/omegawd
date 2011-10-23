56d2b90f91f18b8494438e8cae6e471e65866c58
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-10-23 13:36:03 -0700

    spfdr

--- spfr_setup.js
+++ spfr_setup.js
@@ -3,7 +3,7 @@ var http = require('http'), fs = require('fs');
 var buff = new Buffer('citizen:peapod2011').toString('base64')
 
 var tags = JSON.parse(fs.readFileSync('./lib/tracklist.json'))
-var tats = ['occupy', '#ows']
+var tats = ['occupy', '#ows', 'occupywallstreet', 'occupychicago']
 tats.forEach(subscribe)
 
 function subscribe (tag){
