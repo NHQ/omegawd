f33b13e73f06c17489cc95b36374a23a7e65743a
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-10-23 13:27:09 -0700

    spfdr

--- spfr_setup.js
+++ spfr_setup.js
@@ -8,7 +8,7 @@ tats.forEach(subscribe)
 
 function subscribe (tag){
 		var spfdr = http.createClient(80, 'superfeedr.com');
-		var dataw = "hub.mode=subscribe&hub.verify=async&hub.topic=http://superfeedr.com/track/"+tag+"&hub.callback=http://74.207.246.247:8001/feed";
+		var dataw = "hub.mode=unsubscribe&hub.verify=async&hub.topic=http://superfeedr.com/track/"+tag+"&hub.callback=http://74.207.246.247:8001/feed";
 		var request = spfdr.request('POST', '/hubbub', {
 			'Host':'superfeedr.com',
 			"Authorization":"basic "+buff,
