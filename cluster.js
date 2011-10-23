1bcc1757219369a96995e2e0baa233e328165468
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-10-22 19:28:44 -0700

    a

--- cluster.js
+++ cluster.js
@@ -1,8 +1,3 @@
 var cluster = require('cluster');
 
-cluster('./ows')
-  .use(cluster.logger('logs'))
-  .use(cluster.stats())
-  .use(cluster.pidfiles('pids'))
-  .use(cluster.cli())
-  .use(cluster.repl(8888));
+cluster('./ows');
