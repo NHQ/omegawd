40c65d9955fdfd9c58912be291b7621e0f50b6ee
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-10-23 14:25:26 -0700

    spfdr

--- spfr_setup.js
+++ spfr_setup.js
@@ -3,10 +3,10 @@ var http = require('http'), fs = require('fs');
 var buff = new Buffer('citizen:peapod2011').toString('base64')
 
 var tags = JSON.parse(fs.readFileSync('./lib/tracklist.json'))
-var tats = ['occupy', 'tumblr.com', '-twitter.com'];
+var tats = ['occupy&-twitter.com'];
 var toots = ['occupy', 'chicago', '-twitter.com'];
-subscribe(tats.join("&"))
-subscribe(toots.join('&'))
+subscribe(encodeURIComponent(tats.join("&")))
+//subscribe(encodeURIComponent(toots.join('&')))
 function subscribe (tag){
 		var spfdr = http.createClient(80, 'superfeedr.com');
 		var dataw = "hub.mode=subscribe&hub.verify=async&hub.topic=http://superfeedr.com/track/"+tag+"&hub.callback=http://74.207.246.247:8001/feed/"+tag;
