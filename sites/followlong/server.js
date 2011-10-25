5d52381eb02125381c60a7d0435eb953bdc3e22f
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-10-25 15:48:51 -0700

    followlong

--- sites/followlong/server.js
+++ sites/followlong/server.js
@@ -66,7 +66,7 @@ function getSesh (req, res, next){
 		res.redirect('/fb');
 	if(req.session._id)
 	{
-		person.findOne({person.secrets.fb_id : req.session._id}, function (err, individual){
+		person.findOne({"secrets.fb_id" : req.session._id}, function (err, individual){
 			if (err){console.log(err);}
 			req.session.regenerate(function(err){
 				console.log(individual);
@@ -512,15 +512,15 @@ app.get('/fb/auth', function (req, res) {
 request.get('https://graph.facebook.com/'+body.id+'/picture?type=large&access_token='+access_token).pipe(fs.createWriteStream('public/person/'+person._id+'/profile.jpg'));
 
 				person.insert({
-	        person.facts.fname : body.first_name,
-	        person.facts.mname : body.middle_name,
-	        person.facts.lname : body.last_name,
-	        person.facts.gender : body.gender, 
-	        person.facts.website : body.website, 
-	        person.secrets.fb_access_token : access_token,
-	        person.secrets.fbx : body.friends.data,
-	        person.secrets.fb_id : body.id.
-					person.wire.feeds= [{feed:'http://www.memeorandum.com/feed.xml', chans:['news']},{feed:'http://www.techmeme.com/feed.xml', chans:['tech']},{feed:'http://news.ycombinator.com/rss', chans:['YcombOverator']}]
+	        facts.fname : body.first_name,
+	        facts.mname : body.middle_name,
+	        facts.lname : body.last_name,
+	        facts.gender : body.gender, 
+	        facts.website : body.website, 
+	        secrets.fb_access_token : access_token,
+	        secrets.fbx : body.friends.data,
+	        secrets.fb_id : body.id.
+					wire.feeds= [{feed:'http://www.memeorandum.com/feed.xml', chans:['news']},{feed:'http://www.techmeme.com/feed.xml', chans:['tech']},{feed:'http://news.ycombinator.com/rss', chans:['YcombOverator']}]
 				}, function(err, res){
 					client.append(body.id, res._id, function(err){})
 					//person.update({_id: res._id}, {$set : {person.facts.portrait:'person/'+person._id+'/profile.jpg'}})
