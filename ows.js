aff0c7db0a80402ddd70404aba861d0ba5093970
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-10-26 12:42:43 -0700

    sockets

--- ows.js
+++ ows.js
@@ -75,7 +75,7 @@ var twit = new twitter({
 				var hashtags = _.intersection(_.map(data.tags, function(e){return e.text.toLowerCase()}), this.tracklist);
 				if(hashtags.length){
 					hashtags.forEach(function(tag){
-						pub.publish(tag+':pub', data)
+						pub.publish(tag+':pub', JSON.stringify(data))
 					});
 					if(data.links.length){
 						this.analyze(hashtags,data)
--- sites/live/public/javascripts/live.js
+++ sites/live/public/javascripts/live.js
@@ -1,4 +1,6 @@
   var socket = io.connect('http://74.207.246.247:8008');
   socket.on('news', function (data) {
     console.log(data);
+		var post = JSON.parse(data);
+		$('body').append(post.txt)
   });
