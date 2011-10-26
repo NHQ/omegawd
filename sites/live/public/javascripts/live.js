925e6c8a06360655806a3b0aec11d5785c00fdf9
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-10-26 14:03:30 -0700

    sockets

--- sites/live/public/javascripts/live.js
+++ sites/live/public/javascripts/live.js
@@ -1,6 +1,6 @@
 function append (datum){
 	var data = JSON.parse(datum);
-	var html = 	'<li class="post"><img class="thumb" src='+data.pic+'></img><p>'+data.txt+'</p></li>';
+	var html = 	'<li class="post"><div class="img"><img class="thumb" src='+data.pic+'></img></div></div class="txt"><p>'+data.txt+'</p></div></li>';
 			$('ul#post').append(html);
 }
 
