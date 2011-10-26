e0d722824e94a6f060cd5d7797e26d58f97e5b49
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-10-26 16:52:30 -0700

    sockets

--- sites/live/public/javascripts/live.js
+++ sites/live/public/javascripts/live.js
@@ -14,7 +14,7 @@ function append (datum){
 		}).join(" ")
 		var cut = data.pic.indexOf("_normal");
 		var pic = data.pic.slice(0, cut)+data.pic.slice(cut+7);
-	var html = 	'<li class="post"><div class="img"><a href='+data.home+'><img src='+pic+'></a></div></div class="txt"><p>'+txt+'</p></div></li>';
+	var html = 	'<li class="post"><div class="img"><a href='+data.home+'><img class="thumb" src='+pic+'></a></div></div class="txt"><p>'+txt+'</p></div></li>';
 			$('ul#post').append(html);
 }
 
