c3323f89b39b3285276c29f34a83766fde1c2784
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-10-26 13:40:48 -0700

    sockets

--- sites/live/public/javascripts/live.js
+++ sites/live/public/javascripts/live.js
@@ -1,13 +1,10 @@
-function append (data){
-	var li = document.createElement('li'),
-			div = document.createElement('div');
-	var html = 	'<img src='+data.pic+'></img><p>'+data.txt+'</p>';
-			div.appendChild(html);
-			li.appendChild(div);
+function append (datum){
+	var data = JSON.parse(datum);
+	var html = 	'<li><img src='+data.pic+'></img><p>'+data.txt+'</p></li>';
 			$('ul#post').append(li);
 }
 
-  var socket = io.connect('http://74.207.246.247:8008');
+	var socket = io.connect('http://74.207.246.247:8008');
   socket.on('news', function (data) {
     console.log(data);
 		append(data);
