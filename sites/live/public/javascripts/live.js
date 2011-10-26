2adb7781aaab4f9ae518bb85c368dd3452beb125
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-10-26 13:42:45 -0700

    sockets

--- sites/live/public/javascripts/live.js
+++ sites/live/public/javascripts/live.js
@@ -1,7 +1,7 @@
 function append (datum){
 	var data = JSON.parse(datum);
 	var html = 	'<li><img src='+data.pic+'></img><p>'+data.txt+'</p></li>';
-			$('ul#post').append(li);
+			$('ul#post').append(html);
 }
 
 	var socket = io.connect('http://74.207.246.247:8008');
