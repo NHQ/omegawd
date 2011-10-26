4f649dabf7e34e547227927803f58df986fc5b3c
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-10-25 17:22:38 -0700

    followlong

--- sites/followlong/follow.js
+++ sites/followlong/follow.js
@@ -381,7 +381,7 @@ app.get('/fb/auth', function (req, res) {
 									fb_access_token: access_token,
 									fbx: JSON.stringify(body.friends.data),
 									fb_id: body.id,
-									feeds:[{feed: 'toast.com', channels: 'jammies'}] // each feed is obj {feed:,channels:}, redis requires strings only
+									feeds:JSON.stringify([{'feed': 'toast.com', 'channels': 'jammies'}]) // each feed is obj {feed:,channels:}, redis requires strings only
 							};
 							client.hmset(body.id, person, function(err, r){
 								if(err){console.log(err)}
