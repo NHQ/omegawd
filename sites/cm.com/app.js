5fa3f661a13293b8c0122a7aacb2f1beb52e83b9
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-10-26 18:16:24 -0700

    sockets

--- sites/cm.com/app.js
+++ sites/cm.com/app.js
@@ -103,6 +103,7 @@ app.get('/occupy', vhost, function(req, res){
 		console.log(tags);
 		if (state.toUpperCase().replace(/-/g, " ") == 'NEW YORK' || state.toUpperCase().replace(/-/g, " ") == 'NY'){tags.push('ows:hotlinks', 'occupywallstreet:hotlinks', 'occupywallst:hotlinks')}
 		client.zunionstore(state+':hotlinks', tags.length, tags, function(e,r){
+			console.log(e,r)
 			client.zrevrangebyscore(state+':hotlinks', '+inf', 1, function(e,r){
 				res.render('links', {
 			    title: 'Occupy Links:'+state,
