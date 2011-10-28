d51ab4d50ed51b19c5e310ec98cc794c6c766284
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-10-27 20:49:22 -0700

    trololol

--- sites/live/app.js
+++ sites/live/app.js
@@ -73,7 +73,8 @@ io.sockets.on('connection', function (socket) {
 	});
 	
 	client.on('message', function (channel, message) {
-		io.sockets.in(channel).emit(message)
+			socket.emit('news', message);
+//		io.sockets.in(channel).emit(message)
 //		socket.broadcast.to(channel).emit(message) // can add channel to the emittance
 	});
 	
