fd6730aa03e4e4ce1a30d0957e16fa7e86f8017e
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-10-29 16:52:04 -0700

    trololo

--- sites/live/public/javascripts/live.js
+++ sites/live/public/javascripts/live.js
@@ -1,7 +1,4 @@
-
 $(window).load(function(e){
-	var reg = new RegExp(/t.co/i), usr = new RegExp(/^@[a-z0-9_]*/i);
-	var following = [];
 	var winx = window.innerWidth
 	,		winy = window.innerHeight;
 	var $container = $('#post');
@@ -13,55 +10,46 @@ $(window).load(function(e){
 	$('#post').css({
 		'left': ((winx-$('body').width())/2)
 	})
-	following.states = store.get("states") || [];
-	following.cities = store.get("cities") || []
-/*	
-	$container.isotope({
-		getSortData:{
-      type : function ( $elem ) {
-    return parseInt( $elem.attr('data-type'));
-  },
-	  layoutMode : 'cellsByColumn',
-	  cellsByRow : {
-	    columnWidth : 600,
-	    rowHeight : 150
-	  },
-		animationOptions: {
-	     duration: 333,
-	     easing: 'linear',
-	     queue: false
-	   }
-	});
-*/	
-	function selectors (){	
+	tuner.init();
+})
+var tuner = Object.create(null);
+tuner = {
+	init: function(){
+		this.reg = new RegExp(/t.co/i);
+		this.usr = new RegExp(/^@[a-z0-9_]*/i);
+		this.following = [];
+		this.select();
+		this.socket = io.connect('http://74.207.246.247:8008');
+		this.socket.on('connect');
+		this.socket.on('news', function(data){this.scrub(data)});
+	},
+	select: function(){
 			$('#states').chosen().change(function(e,r){
-			var selects = _.map($('#form').serializeArray(), function(e){return e.value});
-			var sub = _.compact(_.difference(_.union(selects, following), following))
-			var unsub = _.compact(_.difference(_.union(selects, following), selects))
-			following = selects;
-			// store.set("states", following) broken
-			if(sub.length){
-				socket.emit('subscribe', sub[0])
-			}
-			if(unsub.length){
-				socket.emit('unsubscribe', unsub[0])
-			}
-	
-		});
+				var selects = _.map($('#form').serializeArray(), function(e){return e.value});
+				var sub = _.compact(_.difference(_.union(selects, tuner.following), tuner.following))
+				var unsub = _.compact(_.difference(_.union(selects, tuner.following), tuner.selects))
+				following = selects;
+				// store.set("states", following) broken
+				if(sub.length){
+					tuner.socket.emit('subscribe', sub[0])
+				}
+				if(unsub.length){
+					tuner.socket.emit('unsubscribe', unsub[0])
+				}
+			});
 		$('#cities').chosen().change(function(e,r){
 			var selects = _.map($('#form').serializeArray(), function(e){return e.value});
-			var sub = _.compact(_.difference(_.union(selects, following), following))
-			var unsub = _.compact(_.difference(_.union(selects, following), selects))
+			var sub = _.compact(_.difference(_.union(selects, tuner.following), tuner.following))
+			var unsub = _.compact(_.difference(_.union(selects, tuner.following), tuner.selects))
 			following = selects;
 			// store.set("cities", following) broken
 
 			if(sub.length){
-				socket.emit('subscribe', sub[0])
+				tuner.socket.emit('subscribe', sub[0])
 			}
 			if(unsub.length){
-				socket.emit('unsubscribe', unsub[0])
+				tuner.socket.emit('unsubscribe', unsub[0])
 			}
-
 		});
 		$('#occupy').change(function(e,r){
 			var selects = _.map($('#form').serializeArray(), function(e){return e.value});
@@ -76,76 +64,59 @@ $(window).load(function(e){
 			if(unsub.length){
 				socket.emit('unsubscribe', unsub[0])
 			}
-
 		});
-	}selectors();
-	function spfdr (data){
+	},
+	spfrd: function(data){
 		var txt = data.summary ? data.summary : data.content;
 		var pic = data.pic;
 		var link = function(){if (data.link.length < 110){return data.link} else return data.link.slice(0,110)+' ...'};
 		var source = link.slice(0,link.indexOf())
 		var html = 	'<li class="post"><h3 class="title">'+data.title+'</h3>Link: <a href='+link+' target="_blank" class="link">'+link.slice(0,51)+' ...'+'</a></li>';
-		$('ul#post').prepend(html);
-
-	}
-	
-	function twtr (data){
+		this.corral(html)
+		//$('ul#post').prepend(html);
+	},
+	twtr: function(data){
 		var text = data.txt.split(" ");
 		var txt = _.map(text, function(e){
-				if(reg.test(e)){
+				if(this.reg.test(e)){
 					return '<a href='+e+' target="_blank">'+e+'</a>'
 				} 
-				else if(usr.test(e)){
+				else if(this.usr.test(e)){
 					return '<a href=http://twitter.com/#!/'+e.slice(1)+' target="_blank">'+e+'</a>'
 				}
 				else return e
 			}).join(" ")
 			var cut = data.pic.indexOf("_normal");
 			var pic = data.pic.slice(0, cut)+data.pic.slice(cut+7);
-		var html = 	'<li class="post"><div class="img"><a href='+data.home+' target="_blank"><img class="thumb" src='+pic+'></a></div></div class="txt"><p>'+txt+'</p></div></li>';
-				$('ul#post').prepend(html);
-	}
-	function scrub (datum){
+			var html = 	'<li class="post"><div class="img"><a href='+data.home+' target="_blank">';
+					html += '<img class="thumb" src='+pic+'></a></div></div class="txt"><p>'+txt+'</p></div></li>';
+			this.corral(html)
+			//$('ul#post').prepend(html);
+	},
+	scrub: function(datum){
 		
 		var data = JSON.parse(datum);
 		if(data.source === 'twtr'){
-			twtr(data.body)
+			this.twtr(data.body)
 		}
 		else if(data.source === 'spfdr'){
-			spfdr(data.body)
+			this.spfdr(data.body)
+		}
+	},
+	pen: [],
+	display: function(count){
+		var display = this.pen.splice(this.pen.length-count, this.pen.length);
+		display.forEach(function(html){
+			$('ul#post').prepend(html);
+		})
+	},
+	corral: function(html){
+		this.pen.unshift(html);
+		if(this.pen.length > 4){
+			$('#next5').css('visibility', 'visible').click(function(){tuner.display(5);this.style.visibility = 'hidden'})
+		}
+		if(this.pen.length > 4){
+			$('#count').html('Display All '+this.pen.length+'New').css('visibility', 'visible').click(function(){tuner.display(this.pen.length);this.style.visibility = 'hidden'})
 		}
 	}
-//	var socket = io.connect('http://127.0.0.1:8008');
-
-	var socket = io.connect('http://74.207.246.247:8008');
-
-  socket.on('news', function (data) {
-		scrub(data);
-  });
-
-// initish
-
-	socket.on('connect', function(){
-		/*
-		if(following.states.length){
-			following.states.forEach(function(e){
-				socket.emit('subscribe', e)
-				$('#'+e.toUpperCase()).select;
-				console.log(e)
-			})
-		}
-		if(following.cities.length){
-			following.cities.forEach(function(e){
-				socket.emit('subscribe', e)
-				$('#'+e.toUpperCase()).select;
-				$('#'+e).selected = true;
-				console.log(e)
-				
-			})
-		}
-		*/
-	})
-})
-
-
-
+}
