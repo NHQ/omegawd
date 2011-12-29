0b976f7379011fb4e94cadac77aabe9f1657e402
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-12-29 11:56:45 -0800

    try

--- ping.js
+++ ping.js
@@ -8,9 +8,7 @@ var express = require('express')
 
 var app = module.exports = express.createServer();
 app.listen(8808);
-console.log('http://localhost:8008');
-var	io = require('socket.io').listen(app);
-io.set('log level', 0);
+
 // Configuration
 
 app.configure(function(){
@@ -41,43 +39,5 @@ var connections = [];
 		connections.push(req.socket.remoteAddress)
 		var data = JSON.stringify({connections: connections})
 		res.write(data);
-  });
-
-  io.sockets.on('connection', function (socket) {	
-		
-		socket.on('inform', function(meta){
-			var meta = meta
-			,		buff = new Buffer(meta.size) // alt = use blockcount * blocksize
-			,		recd = 0
-			,		expecting = meta.blockCount
-			,		blockSize = meta.blockSize
-			;
-			console.log(meta)
-			this.on(meta.name, function(data){
-				++recd;
-				var boofer = new Buffer(data[2])
-				var uInt8Array = new Uint32Array(data[2])
-				var data = new DataView(event.target.result);
-				boofer.copy(buff, data[0])
-				if(recd === expecting)
-				console.log(recd, expecting, buff.toString('utf8'));
-			})
-			
-			this.emit('copy:'+meta.name)
-		});
-		
-  	socket.on('blob', function(i, data){
-  		if(typeof data === 'object'){
-  		}
-  		if(Array.isArray(data)){
-  			var buff = new Buffer(data[1]);
-  			console.log(buff.toString('utf8'))
-
-  	//		buff.writeUInt8(data, xeon - 1);
-  	//		console.log(buff.toString('utf8', xeon, xeon + 1 ))
-  		}
-  		this.emit('conf', 'aye aye!')
-  	});
-
   });
 
